c2333deb59edbc3b8de7b138d7e66c4ca8a9c7c1
Rokas <rookas.rudzenskas@gmail.com>
2021-06-07 12:40:37 +0300

    Rokas

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8001",
+    baseURL: "https://rookas-tinder-mern-backend.herokuapp.com",
 });
 
 export default instance;
