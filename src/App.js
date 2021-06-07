ffa09e95a626128483da2554689b2f01d0a3112b
Rokas <rookas.rudzenskas@gmail.com>
2021-06-07 10:36:30 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from "./components/Header";
 import TinderCard from "react-tinder-card";
 import TinderCards from "./components/TinderCards";
+import SwipeButtons from "./components/SwipeButtons";
 
 function App() {
   return (
     <div className="app">
         <Header />
         <TinderCards />
+        <SwipeButtons />
     </div>
   );
 }
---
+++ src/components/SwipeButtons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles/SwipeButtons.css";
+
+const SwipeButtons = () => {
+    return (
+        <div className="swipeButtons">
+            
+        </div>
+    );
+};
+
+export default SwipeButtons;
