b52b4b70a16f76f5253f7befaea5fe004f6e8dea
Rokas <rookas.rudzenskas@gmail.com>
2021-06-07 10:27:24 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from "./components/Header";
+import TinderCard from "react-tinder-card";
+import TinderCards from "./components/TinderCards";
 
 function App() {
   return (
     <div className="app">
         <Header />
+        <TinderCards />
     </div>
   );
 }
--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./styles/TinderCards.css";
+import TinderCard from "react-tinder-card";
 
 const TinderCards = () => {
+
+    const [people, setPeople] = useState([
+        {
+            name: "Elon Musk",
+            url: "https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg",
+        },
+
+        {
+            name: "Jeff Bezos",
+            url: "https://api.time.com/wp-content/uploads/2020/07/jeff-bezos.jpg",
+        },
+
+
+    ]);
+
+    const swiped = (direction, nameToDelete) => {
+        console.log("removing" + nameToDelete);
+        // setLastDirection(direction);
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + "left on screen!");
+    };
+
     return (
         <div className="tinderCards">
+            <div className="tinderCards__cardContainer">
+                {people.map((person) => (
 
+                    <TinderCard
+                        className="swipe"
+                        key={person.name}
+                        preventSwipe={['up', 'down']}
+                        onSwipe={(dir) => swiped(dir, person.name)}
+                        onCardLeftScreen={() => outOfFrame(person.name)}
+                    >
+
+                        <div className="card" style={{ backgroundImage: "url(" + person.url + ")" }}>
+                            <h3>{person.name}</h3>
+                        </div>
+
+                    </TinderCard>
+                ))}
+            </div>
         </div>
     );
 };
