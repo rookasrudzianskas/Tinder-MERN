6de8ecb6e4cd725fbe50b61cd3739c3f01bd0567
Rokas <rookas.rudzenskas@gmail.com>
2021-06-07 10:11:48 +0300

    Roaks

---
+++ src/components/TinderCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./styles/TinderCards.css";
+
+const TinderCards = () => {
+    return (
+        <div className="tinderCards">
+
+        </div>
+    );
+};
+
+export default TinderCards;
