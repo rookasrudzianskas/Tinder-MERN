26c273bc2ef21f30ad1cecfb5f640a03e89711db
Rokas <rookas.rudzenskas@gmail.com>
2021-06-07 10:41:38 +0300

    Rokas:

--- src/components/SwipeButtons.js
+++ src/components/SwipeButtons.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import "./styles/SwipeButtons.css";
+import ReplayIcon from "@material-ui/icons/Replay";
+import StarRateIcon from "@material-ui/icons/StarRate";
+import CloseIcon from "@material-ui/icons/Close";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import IconButton from "@material-ui/core/IconButton";
 
 const SwipeButtons = () => {
     return (
         <div className="swipeButtons">
-            
+            <IconButton className="swipeButtons__repeat">
+                <ReplayIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__left">
+                <CloseIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__star">
+                <StarRateIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__right">
+                <FavoriteIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__lightning">
+                <FlashOnIcon fontSize="large" />
+            </IconButton>
         </div>
     );
 };
